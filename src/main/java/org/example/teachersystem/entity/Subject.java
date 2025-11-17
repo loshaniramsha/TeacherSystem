6a103625c60b46635a76d422acb7c4e7174c7239
Loshani Ramsha <loshani.r@boswingroup.com>
2025-11-18 01:34:18 +0530

    feat(entity): add Subject entity with JPA annotations

--- src/main/java/org/example/teachersystem/entity/Subject.java
+++ src/main/java/org/example/teachersystem/entity/Subject.java
@@ -1,5 +1,20 @@
 package org.example.teachersystem.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "subject")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class Subject {
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+    private int subjectId;
+   private String subjectName;
+
 }
