059bf4b322c0cefe34c5613fabdbb8def75f6ab8
Loshani Ramsha <loshani.r@boswingroup.com>
2025-11-18 01:39:54 +0530

    feat(entity): add Teacher entity with JPA annotations

--- src/main/java/org/example/teachersystem/entity/Teacher.java
+++ src/main/java/org/example/teachersystem/entity/Teacher.java
@@ -1,4 +1,22 @@
 package org.example.teachersystem.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "teacher")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int teacher_id;
+    private String teacher_name;
+    private String teacher_email;
+    private String password;
+
 }
