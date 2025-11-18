052743140ca63f11798a60fac838926f718cce3f
Loshani Ramsha <loshani.r@boswingroup.com>
2025-11-18 12:18:08 +0530

    feat(MarksDTO): add save, update, getAll, getByStudent, and deleteMarks methods

--- src/main/java/org/example/teachersystem/service/MarksService.java
+++ src/main/java/org/example/teachersystem/service/MarksService.java
@@ -1,4 +1,14 @@
 package org.example.teachersystem.service;
 
+import org.example.teachersystem.dto.MarksDTO;
+
+import java.util.List;
+
 public interface MarksService {
+    MarksDTO saveMarks(MarksDTO marksDTO);
+    MarksDTO updateMarks(MarksDTO marksDTO);
+    List<MarksDTO> getAllMarks();
+    void deleteMarks(int marksId);
+    List<MarksDTO> getMarksByStudent(int studentId);
+
 }
