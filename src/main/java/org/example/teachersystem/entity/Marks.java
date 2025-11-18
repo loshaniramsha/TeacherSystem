04a2fecc4fe866618f37bc6e7edb25a58a628fd4
Loshani Ramsha <loshani.r@boswingroup.com>
2025-11-18 11:50:44 +0530

    feat(entity): add Marks entity with JPA annotations

--- src/main/java/org/example/teachersystem/entity/Marks.java
+++ src/main/java/org/example/teachersystem/entity/Marks.java
@@ -1,4 +1,27 @@
 package org.example.teachersystem.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "marks")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Marks {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "student_subject_id")
+    private StudentSubject studentSubject;
+
+    @ManyToOne
+    @JoinColumn(name = "teacher_id")
+    private Teacher teacher;
+
 }
